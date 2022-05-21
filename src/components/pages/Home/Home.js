f47ebf108af3e1e0d01cab5102ac2bc1959f723d
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-22 01:30:51 +0600

    tailwind css and daisyUI install done

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <h1 className='text-center mt-5 text-xl'>Welcome to Home</h1>
+        </section>
     );
 };
 
