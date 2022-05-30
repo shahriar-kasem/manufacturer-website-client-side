62e79964934fd17ddf93d15bad530c2d7fa02dd5
Shahriar Kasem <shahriarkasem@gmail.com>
2022-05-30 20:37:09 +0600

    email verification import done

--- src/components/shared/RequireAuth/RequireAuth.js
+++ src/components/shared/RequireAuth/RequireAuth.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../Loading/Loading';
 
 const RequireAuth = ({ children }) => {
   const [user, loading] = useAuthState(auth);
   const location = useLocation();
+  const [sendEmailVerification] = useSendEmailVerification(auth);
 
   if (loading) {
     return <Loading></Loading>
